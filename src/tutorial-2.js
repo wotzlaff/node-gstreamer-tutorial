456d70e4babc3a0cf181b725fc754315a7e9c19c
Nico Strasdat <nstrasdat@gmail.com>
2020-05-15 12:36:27 +0200

    Add tutorial-2

---
+++ src/tutorial-2.js
@@ -0,0 +1,49 @@
+const gi = require('node-gtk')
+const Gst = gi.require('Gst', '1.0')
+gi.startLoop()
+Gst.init()
+
+const pipeline = new Gst.Pipeline()
+const src = Gst.ElementFactory.make('videotestsrc')
+const sink = Gst.ElementFactory.make('autovideosink')
+if (!pipeline || !src || !sink) {
+  throw new Error('Not all elements could be created.')
+}
+
+// In the original example `gst_bin_add_many` is used...
+pipeline.add(src)
+pipeline.add(sink)
+if (!src.link(sink)) {
+  pipeline.unref()
+  throw new Error('Elements could not be linked.')
+}
+
+src.pattern = 0
+
+const ret = pipeline.setState(Gst.State.PLAYING)
+if (ret === Gst.State.CHANGE_FAILURE) {
+  pipeline.unref()
+  throw new Error('Unable to set the pipelne to the playing state.')
+}
+
+const bus = pipeline.getBus()
+const msg =  bus.timedPopFiltered(Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+if (msg) {
+  if (msg.type & Gst.MessageType.ERROR) {
+    console.log('Got error')
+    // Something like this should work to obtain details:
+    // const [err, debug] = msg.parseError()
+    // console.error(`Error received from element ${msg.src}: ${msg.message}`)
+    // err.clear()
+  } else if (msg.type & Gst.MessageType.EOS) {
+    console.log('End-Of-Stream reached.')
+  } else {
+    console.error('Unexpected message received.')
+  }
+  // We should probably do this, but unref is not yet available:
+  // msg.unref()
+}
+
+bus.unref()
+pipeline.setState(Gst.State.NULL)
+pipeline.unref()
