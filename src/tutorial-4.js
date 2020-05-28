6debd41ce192f7d1e3ad63063c1aac7d7368ecfb
Nico Strasdat <nstrasdat@gmail.com>
2020-05-28 11:27:28 +0200

    Wrap into main function to enable return

--- src/tutorial-4.js
+++ src/tutorial-4.js
@@ -12,67 +12,70 @@ let playing = false
 let seekEnabled = false
 let seekDone = false
 
+let playbin = null
+
 // Initialize GStreamer
 Gst.init()
 
-// Create the elements
-const playbin = Gst.ElementFactory.make('playbin')
-if (!playbin) {
-  console.error('Could not create playbin')
-  return
-}
+function main() {
+  // Create the elements
+  playbin = Gst.ElementFactory.make('playbin')
+  if (!playbin) {
+    console.error('Could not create playbin')
+    return
+  }
 
-// Set the URI to play
-gi._c.ObjectPropertySetter(playbin, 'uri', 'https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm')
+  // Set the URI to play
+  gi._c.ObjectPropertySetter(playbin, 'uri', 'https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm')
 
-// Start playing
-if (playbin.setState(Gst.State.PLAYING) === Gst.State.CHANGE_FAILURE) {
-  console.error('Unable to set the pipeline to the playing state.')
-  playbin.unref()
-  return
-}
+  // Start playing
+  if (playbin.setState(Gst.State.PLAYING) === Gst.State.CHANGE_FAILURE) {
+    console.error('Unable to set the pipeline to the playing state.')
+    playbin.unref()
+    return
+  }
 
-// Listen to the bus
-const bus = playbin.getBus()
-while (!terminate) {
-  const msg = bus.timedPopFiltered(100 * Gst.MSECOND, Gst.MessageType.STATE_CHANGED | Gst.MessageType.ERROR | Gst.MessageType.EOS | Gst.MessageType.DURATION_CHANGED)
-  if (msg) {
-    handleMessage(msg)
-  } else {
-    // We got no message, this means the timeout expired
-    if (!playing) continue
+  // Listen to the bus
+  const bus = playbin.getBus()
+  while (!terminate) {
+    const msg = bus.timedPopFiltered(100 * Gst.MSECOND, Gst.MessageType.STATE_CHANGED | Gst.MessageType.ERROR | Gst.MessageType.EOS | Gst.MessageType.DURATION_CHANGED)
+    if (msg) {
+      handleMessage(msg)
+    } else {
+      // We got no message, this means the timeout expired
+      if (!playing) continue
 
-    // Query the current position of the stream
-    const [ok, current] = playbin.queryPosition(Gst.Format.TIME)
-    if (!ok) {
-      console.error('Could not query current position.')
-    }
-
-    // If we didn't know it yet, query the stream duration
-    if (duration !== Gst.CLOCK_TIME_NONE) {
-      const [ok, durationValue] = playbin.queryDuration(Gst.Format.TIME)
+      // Query the current position of the stream
+      const [ok, current] = playbin.queryPosition(Gst.Format.TIME)
       if (!ok) {
-        console.error('Could not query current duration.')
+        console.error('Could not query current position.')
       }
-      duration = durationValue
-    }
 
-    // Print current position and total duration
-    console.log(`Position ${current} / ${duration}`)
+      // If we didn't know it yet, query the stream duration
+      if (duration !== Gst.CLOCK_TIME_NONE) {
+        const [ok, durationValue] = playbin.queryDuration(Gst.Format.TIME)
+        if (!ok) {
+          console.error('Could not query current duration.')
+        }
+        duration = durationValue
+      }
 
-    // If seeking is enabled, we have not done it yet, and the time is right, seek
-    if (seekEnabled && !seekDone && current > 10 * Gst.SECOND) {
-      console.log('Reached 10s, performing seek...')
-      playbin.seekSimple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT, 30 * Gst.SECOND)
-      seekDone = true
+      // Print current position and total duration
+      console.log(`Position ${current} / ${duration}`)
+
+      // If seeking is enabled, we have not done it yet, and the time is right, seek
+      if (seekEnabled && !seekDone && current > 10 * Gst.SECOND) {
+        console.log('Reached 10s, performing seek...')
+        playbin.seekSimple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT, 30 * Gst.SECOND)
+        seekDone = true
+      }
     }
   }
+
+  bus.unref()
+  pipeline.setState(Gst.State.NULL)
+  pipeline.unref()
 }
-
-bus.unref()
-pipeline.setState(Gst.State.NULL)
-pipeline.unref()
-
 
 function handleMessage(msg) {
   switch (msg.type) {
@@ -115,3 +118,5 @@ function handleMessage(msg) {
       break
   }
 }
+
+main()
--- src/tutorial-6.js
+++ src/tutorial-6.js
@@ -10,82 +10,84 @@ let terminate = false
 // Initialize GStreamer
 Gst.init()
 
-// Create the element factories
-const srcFactory = Gst.ElementFactory.find('audiotestsrc')
-const sinkFactory = Gst.ElementFactory.find('autoaudiosink')
-if (!srcFactory || !sinkFactory) {
-  console.error('Not all element factories could be created.')
-  return
-}
-
-// Print information about the pad templates of these factories
-printPadTemplateInformation(srcFactory)
-printPadTemplateInformation(sinkFactory)
-
-// Ask the factories to instantiate actual elements
-const src = srcFactory.create()
-const sink = sinkFactory.create()
-
-// Create the empty pipeline
-const pipeline = new Gst.Pipeline()
-
-if (!pipeline || !src || !sink) {
-  console.error('Not all elements could be created.')
-  return
-}
-
-// Build the pipeline
-pipeline.add(src)
-pipeline.add(sink)
-if (!src.link(sink)) {
-  console.error('Elements could not be linked.')
-  pipeline.unref()
-  return
-}
-
-// Print initial negotiated caps (in NULL state)
-console.log('In NULL state:')
-printPadCapabilities(sink, 'sink')
-
-// Start playing
-if (pipeline.setState(Gst.State.PLAYING) === Gst.State.CHANGE_FAILURE) {
-  console.error('Unable to set the pipeline to the playing state (check the bus for error messages).')
-}
-
-// Listen to the bus
-const bus = pipeline.getBus()
-while (!terminate) {
-  const msg = bus.timedPopFiltered(Gst.CLOCK_TIME_NONE, Gst.MessageType.STATE_CHANGED | Gst.MessageType.ERROR | Gst.MessageType.EOS)
-  if (!msg) continue
-
-  switch (msg.type) {
-    case Gst.MessageType.ERROR:
-      console.log('Got error.')
-      // TOOD: parse error
-      terminate = true
-      break
-    case Gst.MessageType.EOS:
-      console.log('End-Of-Stream reached.')
-      terminate = true
-      break
-    case Gst.MessageType.STATE_CHANGED:
-      if (msg.src === pipeline) {
-        const [oldState, newState, pendingState] = msg.parseStateChanged()
-        console.log(`Pipeline state changed from ${Gst.Element.stateGetName(oldState)} to ${Gst.Element.stateGetName(newState)}.`)
-        printPadCapabilities(sink, 'sink')
-      }
-      break
-    default:
-      console.error('Unknown message type:', msg)
-      break
+function main() {
+  // Create the element factories
+  const srcFactory = Gst.ElementFactory.find('audiotestsrc')
+  const sinkFactory = Gst.ElementFactory.find('autoaudiosink')
+  if (!srcFactory || !sinkFactory) {
+    console.error('Not all element factories could be created.')
+    return
   }
-}
 
-bus.unref()
-pipeline.setState(Gst.State.NULL)
-pipeline.unref()
-srcFactory.unref()
-sinkFactory.unref()
+  // Print information about the pad templates of these factories
+  printPadTemplateInformation(srcFactory)
+  printPadTemplateInformation(sinkFactory)
+
+  // Ask the factories to instantiate actual elements
+  const src = srcFactory.create()
+  const sink = sinkFactory.create()
+
+  // Create the empty pipeline
+  const pipeline = new Gst.Pipeline()
+
+  if (!pipeline || !src || !sink) {
+    console.error('Not all elements could be created.')
+    return
+  }
+
+  // Build the pipeline
+  pipeline.add(src)
+  pipeline.add(sink)
+  if (!src.link(sink)) {
+    console.error('Elements could not be linked.')
+    pipeline.unref()
+    return
+  }
+
+  // Print initial negotiated caps (in NULL state)
+  console.log('In NULL state:')
+  printPadCapabilities(sink, 'sink')
+
+  // Start playing
+  if (pipeline.setState(Gst.State.PLAYING) === Gst.State.CHANGE_FAILURE) {
+    console.error('Unable to set the pipeline to the playing state (check the bus for error messages).')
+  }
+
+  // Listen to the bus
+  const bus = pipeline.getBus()
+  while (!terminate) {
+    const msg = bus.timedPopFiltered(Gst.CLOCK_TIME_NONE, Gst.MessageType.STATE_CHANGED | Gst.MessageType.ERROR | Gst.MessageType.EOS)
+    if (!msg) continue
+
+    switch (msg.type) {
+      case Gst.MessageType.ERROR:
+        console.log('Got error.')
+        // TOOD: parse error
+        terminate = true
+        break
+      case Gst.MessageType.EOS:
+        console.log('End-Of-Stream reached.')
+        terminate = true
+        break
+      case Gst.MessageType.STATE_CHANGED:
+        if (msg.src === pipeline) {
+          const [oldState, newState, pendingState] = msg.parseStateChanged()
+          console.log(`Pipeline state changed from ${Gst.Element.stateGetName(oldState)} to ${Gst.Element.stateGetName(newState)}.`)
+          printPadCapabilities(sink, 'sink')
+        }
+        break
+      default:
+        console.error('Unknown message type:', msg)
+        break
+    }
+  }
+
+  bus.unref()
+  pipeline.setState(Gst.State.NULL)
+  pipeline.unref()
+  srcFactory.unref()
+  sinkFactory.unref()
+}
 
 function printPadTemplateInformation(factory) {
   console.log(`Pad Templates for ${factory.getName()}:`)
@@ -181,3 +183,5 @@ function printPadCapabilities(element, padName) {
   // caps.unref()
   pad.unref()
 }
+
+main()
